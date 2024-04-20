3ab63f42d8ccf70155661de1d9cccfb95a0241ab
Pabluwu707 <pabloshiny@gmail.com>
2024-04-20 23:55:42 +0200

    Animacion de bici y otros

--- scripts/UI.gd
+++ scripts/UI.gd
@@ -1,17 +1,21 @@
 extends Node2D
 
-#@export var VidaCounter : TextEdit
-#@export var MoneyCounter : TextEdit
 @onready var VidaCounter := $VidaDisplay
 @onready var MoneyCounter := $DineroDisplay
+@onready var DistanciaCounter := $DistanciaDisplay/ProgressBar
+
+@export var Player : CharacterBody2D
+@export var Meta : Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func setup_ui(maxlife, currentmoney):
+func setup_ui(maxlife, currentmoney, intialposition):
 	VidaCounter.setup_vida(maxlife)
 	MoneyCounter.setup_dinero(currentmoney)
+	DistanciaCounter.setup_distancia(Player, Meta, intialposition)
+	
 
 func change_vida(value):
 	VidaCounter.change_vida(value)
