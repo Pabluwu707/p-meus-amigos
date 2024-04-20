552cdcb4f2901bc1ed85e64c2c204501cbd43475
Pablo Millán Cubero <pablomillancb@gmail.com>
2024-04-20 20:32:22 +0200

    Comet collision fixed fr fr:sob:

--- scripts/CometHazard.gd
+++ scripts/CometHazard.gd
@@ -1,5 +1,4 @@
-extends RigidBody2D
-
+extends CharacterBody2D
 
 const SPEED = 250
 var rng = RandomNumberGenerator.new()
@@ -9,6 +8,9 @@ var direction = Vector2(0, 1).rotated(random_rotation)
 func _ready():
 	global_rotation = random_rotation
 	#velocity = direction
-	gravity_scale = 0
-	add_constant_central_force(Vector2.DOWN)
-
+	#gravity_scale = 0
+	#add_constant_central_force(direction * SPEED)
+	
+func _physics_process(delta):
+	#move_and_collide(direction*SPEED)
+	move_and_collide(direction*SPEED*delta)
