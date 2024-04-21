3d858ff83a78965ef741904bf9f0c675b657667a
Pablo Millán Cubero <pablomillancb@gmail.com>
2024-04-21 11:36:44 +0200

    Aseprite imported

--- scripts/warning_sign.gd
+++ scripts/warning_sign.gd
@@ -15,10 +15,6 @@ func spawn_car():
 	car.global_position.x = get_parent().get_parent().get_node("Player").global_position.x + 900
 	queue_free()
 
-func _on_flicker_timeout():
-	if (running):
-		%Sign.visible = !%Sign.visible
-
 
 func _on_spawn_timer_timeout():
 	running = false
