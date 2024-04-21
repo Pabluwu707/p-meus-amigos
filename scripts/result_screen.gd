0ddb26203e2509817b55d0abd707d8df8218e226
Pablo Millán Cubero <pablomillancb@gmail.com>
2024-04-21 17:33:41 +0200

    Result screen finished

--- scripts/result_screen.gd
+++ scripts/result_screen.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
-func on_create(alien: String, reward, damage, sentence):
+func on_create(alien: String, reward, damage, sentence, portrait):
 	%Name.text = alien
 	%Reward.text = str(reward)
 	%Damage.text = str(damage)
 	%Total.text = str(reward - damage)
 	%Sentence.text = sentence
-
+	var portrait_node = load("res://scenes/" + portrait +  ".tscn").instantiate()
+	add_child(portrait_node)
+	portrait_node.position = %PortraitPlace.position
 func _ready():
-	#on_create("Xokulon", 100, 25, "qe dise")
+	#on_create("Xokulon", 100, 25, "qe dise", "asteroid_hazard")
 	pass
